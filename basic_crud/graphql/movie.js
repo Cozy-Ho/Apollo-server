58f511f53cb1b38f195535a099f24df997e17011
cozy-ho <kj2693119@gmail.com>
2021-01-11 18:02:48 +0900

    [Mod] 코드 수정중 임시 commit

--- basic_crud/graphql/movie.js
+++ basic_crud/graphql/movie.js
@@ -1,48 +1,47 @@
 import Movie from "../models/movies";
 
-function insertTestDB() {
-  Movie.insertMany([
-    {
-      title: "test1",
-      score: 1,
-    },
-    {
-      title: "test2",
-      score: 2,
-    },
-    {
-      title: "test3",
-      score: 3,
-    },
-    {
-      title: "test4",
-      score: 54,
-    },
-    {
-      title: "jan",
-      score: 40,
-    },
-    {
-      title: "fev",
-      score: 71,
-    },
-    {
-      title: "dev",
-      score: 90,
-    },
-    {
-      title: "feba",
-      score: 39,
-    },
-  ])
-    .then((res) => {
-      console.log(res);
-      return true;
-    })
-    .catch((err) => {
-      console.log(err);
-      return false;
-    });
+async function insertTestDB() {
+  try {
+    await Movie.insertMany([
+      {
+        title: "test1",
+        score: 1,
+      },
+      {
+        title: "test2",
+        score: 2,
+      },
+      {
+        title: "test3",
+        score: 3,
+      },
+      {
+        title: "test4",
+        score: 54,
+      },
+      {
+        title: "jan",
+        score: 40,
+      },
+      {
+        title: "fev",
+        score: 71,
+      },
+      {
+        title: "dev",
+        score: 90,
+      },
+      {
+        title: "feba",
+        score: 39,
+      },
+    ]);
+
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 }
 
 function getMovies() {
@@ -65,7 +64,17 @@ function getByTitle(title) {
   }
 }
 
-function updateMovie(title, score = null, update_title = null) {
+async function updateMovie(title, score = null, update_title = null) {
+  // Todo. ID 값 추가해서 쉽게 가도록 고치자.
+  //
+  // try{
+  //   const updateMovie = await Movie.findOneAndUpdate({
+  //     title: title,
+  //   });
+  //   const updatedMovie = await Movie.Update({})
+  // }catch(err){
+  //   //
+  // }
   const updatedMovie = Movie.findOne(
     {
       title: title,
@@ -93,25 +102,22 @@ function updateMovie(title, score = null, update_title = null) {
   return updatedMovie;
 }
 
-function deleteMovie(title) {
-  Movie.findOneAndDelete(
-    {
+async function deleteMovie(title) {
+  try {
+    const movie = await Movie.findOneAndDelete({
       title: title,
-    },
-    function (err, movie) {
-      if (err) {
-        console.log(err);
-        return false;
-      }
-      console.log(movie);
-      return true;
-    }
-  );
+    });
+    console.log(movie);
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 }
 
-function addMovie(title, score) {
+async function addMovie(title, score) {
   try {
-    const addedMovie = Movie.create({
+    const addedMovie = await Movie.create({
       title: title,
       score: score,
     });
@@ -121,16 +127,14 @@ function addMovie(title, score) {
     throw err;
   }
 }
-function deleteAll() {
-  Movie.remove({})
-    .then((res) => {
-      console.log(res);
-      return true;
-    })
-    .catch((err) => {
-      console.log(err);
-      return false;
-    });
+async function deleteAll() {
+  try {
+    await Movie.remove({});
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 }
 
 module.exports = {
